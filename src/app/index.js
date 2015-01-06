eb65ddfd2f5cf5d8534949b1d5bd3b300e6284bb
Blake Bourque <Techplex.Engineer@gmail.com>
2015-01-05 21:04:13 -0500

    Scaffoleded app

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('easyj', ['ngResource', 'ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
---
+++ src/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('easyj')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
