eaef37b5fd1add8ce07b5dd91aec2ef7f4bdf0a3
Blake Bourque <Techplex.Engineer@gmail.com>
2015-01-05 21:43:35 -0500

    Make the navbar hilight the page we are on

--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('easyj')
-  .controller('NavbarCtrl', function ($scope) {
+  .controller('NavbarCtrl', function ($scope, $location) {
     $scope.date = new Date();
+    var nav = this;
+
+    nav.isActive = function (viewLocation) {
+      return viewLocation == $location.path();
+    };
   });
