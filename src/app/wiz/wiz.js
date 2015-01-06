f61795c0b81ae6d3566ff11f1e4536c9d6d31756
Blake Bourque <Techplex.Engineer@gmail.com>
2015-01-06 00:02:05 -0500

    Bringing over the views

--- src/app/wiz/wiz.js
+++ src/app/wiz/wiz.js
@@ -1,31 +1,53 @@
 'use strict';
 
-angular.module('easyj.wiz', ['ngResource', 'ngRoute'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/wizard', {
-        templateUrl: 'app/wiz/wiz.html',
-        controller: 'WizCtrl'
-      })
-      .when('/wizard/:step', {
-        templateUrl: function ($routeParams) {
-          return 'app/wiz/steps/wiz_steps.html';
-        },
-        controller: 'WizCtrl',
-        controllerAs: 'wiz'
-      });
-  })
-;
+var app = angular.module('easyj.wiz', ['ngResource', 'ngRoute'])
 
-var app = angular.module('easyj.wiz');
-
-app.controller('WizCtrl', function ($scope, $routeParams) {
-  this.step = $routeParams.step;
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/wizard', {
+      templateUrl: 'app/wiz/wiz.html',
+      controller: 'WizCtrl'
+    })
+    .when('/wizard/:step', {
+      templateUrl: function ($routeParams) {
+        return 'app/wiz/steps/wiz_steps.html';
+      },
+      controller: 'WizCtrl',
+      controllerAs: 'wiz'
+    });
 });
 
-app.controller('Wiz1Ctrl', function ($scope, $routeParams) {
-  console.log("Wiz1Ctrl loaded");
-  $scope.test = $routeParams;
+app.factory('RobotLimits', function() {
+  console.log("Robot Limits factory");
+  var limits = {
+    PWM: 10,
+    Sol: 8,
+    ds: {
+      USB: 4
+    },
+    sensors: {
+      analog: 4,
+      digital: 10
+    },
+    relay: 4
+  };
+  return limits;
+});
+
+app.factory('RobotData', function(){
+  console.log("Robot Data factory");
+  return {name:"test"};
+});
+
+
+
+app.controller('WizCtrl', function ($scope, $routeParams) {
+  this.step = $routeParams.step; //this is used to determine what template to load
+});
+
+app.controller('Wiz1Ctrl', function (RobotData) {
+  console.log("Wiz1Ctrl loaded", RobotData);
+
 });
 
 app.controller('Wiz2Ctrl', function ($scope) {
