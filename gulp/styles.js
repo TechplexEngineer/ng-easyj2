9a8c35a5a5872e3799d3cefe9c2527db853d4a0d
Blake Bourque <techplex.engineer@gmail.com>
2016-03-27 23:35:39 -0400

    Properly build styles

--- gulp/styles.js
+++ gulp/styles.js
@@ -28,29 +28,12 @@ var buildStyles = function() {
     ]
   };
 
-  var injectFiles = gulp.src([
-    path.join(conf.paths.src, '/app/**/*.less'),
-    path.join('!' + conf.paths.src, '/app/index.less')
-  ], { read: false });
-
-  var injectOptions = {
-    transform: function(filePath) {
-      filePath = filePath.replace(conf.paths.src + '/app/', '');
-      return '@import "' + filePath + '";';
-    },
-    starttag: '// injector',
-    endtag: '// endinjector',
-    addRootSlash: false
-  };
-
-
   return gulp.src([
-    path.join(conf.paths.src, '/app/index.less')
+  	path.join(conf.paths.src, '/app/**/*.css'),
+    path.join(conf.paths.src, '/app/app.css')
   ])
-    .pipe($.inject(injectFiles, injectOptions))
     .pipe(wiredep(_.extend({}, conf.wiredep)))
     .pipe($.sourcemaps.init())
-    .pipe($.less(lessOptions)).on('error', conf.errorHandler('Less'))
     .pipe($.autoprefixer()).on('error', conf.errorHandler('Autoprefixer'))
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/app/')));
